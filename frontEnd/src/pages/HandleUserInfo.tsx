e96b46e13954f8e7bbff72cf8fdaaf4b23478d4f
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-12-28 14:39:45 +0200

    Enhance user info page layout and add images
    Updated HandleUserInfo.tsx with a new landing section featuring a headline, description, call-to-action, and image. Added home.jpg and home1.jpg assets to the public directory for improved visual presentation.

--- frontEnd/src/pages/HandleUserInfo.tsx
+++ frontEnd/src/pages/HandleUserInfo.tsx
@@ -1,9 +1,24 @@
 
 const HandleUserInfo = () => {
   return (
-    <div>
-      Welcome to the User Info Page
-    </div>
+    <section className="bg-gray-900 h-screen justify-center items-center flex overflow-hidden">
+        <div className="grid max-w-7xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7">
+                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">Job Application Tracker</h1>
+                <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400">Job hunting is already hard enough. But now you can keep every job application, interview, and follow-up in one place so you can stay organized, prepared, and confident throughout your job search. It won't make it any easier, but it will help you stay on top of everything.</p>
+                <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900">
+                    Get started
+                    <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            <span className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border rounded-lg focus:ring-4 text-white border-gray-700 hover:bg-gray-700 focus:ring-gray-800">
+                I love cats 😁🤣
+            </span> 
+            </div>
+            <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <img src="home1.jpg" className="h-175 ml-10 w-6xl rounded-lg" alt="mockup" />
+            </div>                
+        </div>
+    </section>
   )
 }
 
