984ca666bc31f164f8057e8cbc3df197f2b6a377
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-12-28 13:46:14 +0200

    Add React Router and basic page routing
    Integrated react-router and react-router-dom for client-side routing. Added Home and HandleUserInfo pages, set up routes in App.tsx, and wrapped the app with BrowserRouter in main.tsx.

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router";
 import './App.css'
+import Home from "./pages/Home";
+import HandleUserInfo from "./pages/HandleUserInfo";
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/login" element={<HandleUserInfo />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ frontEnd/src/pages/HandleUserInfo.tsx
@@ -0,0 +1,10 @@
+
+const HandleUserInfo = () => {
+  return (
+    <div>
+      Welcome to the User Info Page
+    </div>
+  )
+}
+
+export default HandleUserInfo
